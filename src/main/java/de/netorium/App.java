d21674e69ae4a53e047e2277175723e39ba0bb0f
Andreas Ries <andreas.ries@netorium.de>
2016-04-04 17:19:21 +0200

    Added mac address feature

--- src/main/java/de/netorium/App.java
+++ src/main/java/de/netorium/App.java
@@ -3,6 +3,10 @@ package de.netorium;
 import org.jutils.jhardware.HardwareInfo;
 import org.jutils.jhardware.model.*;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.Map;
 
 public class App
@@ -44,7 +48,39 @@ public class App
             System.out.println(e.getMessage());
         }
 
+        getNetworkMac();
+
         System.out.println( "Hello World!" );
+    }
+
+    private static void getNetworkMac() {
+        InetAddress ip;
+        try {
+
+            ip = InetAddress.getLocalHost();
+            System.out.println("Current IP address : " + ip.getHostAddress());
+
+            NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+
+            byte[] mac = network.getHardwareAddress();
+
+            System.out.print("Current MAC address : ");
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < mac.length; i++) {
+                sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+            }
+            System.out.println(sb.toString());
+
+        } catch (UnknownHostException e) {
+
+            e.printStackTrace();
+
+        } catch (SocketException e){
+
+            e.printStackTrace();
+
+        }
     }
 
     private static void printComponentInfo(String clazz, Map<String, String> info) {
